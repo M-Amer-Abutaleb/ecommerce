41e7abc983db7a4ec78341bf57c241393a8a02c4
Mohamed Amer <m.amer_abutlabe@hotmail.com>
2022-06-25 08:32:13 +0200

    setting the home page

---
+++ src/context/index.js
@@ -0,0 +1,6 @@
+import { ProductsProvider } from "./products_context";
+import { CartProvider } from "./cart_context";
+import { UserProvider } from "./user_context";
+import { FilterProvider } from "./filter_context";
+
+export { ProductsProvider, CartProvider, UserProvider, FilterProvider };
